cf0ba7595cf6d99d67f5b034d71609bfefc0f3ae
Pau Estalella <pau.estalella@gmail.com>
2025-05-27 20:28:11 +0200

    Start decoding ADDI
    Very initial code to decode ADDI.

--- src/execution_unit.sv
+++ src/execution_unit.sv
@@ -30,6 +30,15 @@ module exec_unit (
 	logic reg_rd1_en;
 	logic reg_wr_en;
 
+	logic [4:0] decode_reg_rd0_addr;
+	logic [4:0] decode_reg_rd1_addr;
+	logic [4:0] decode_reg_wr_addr;
+	logic decode_reg_rd0_en;
+	logic decode_reg_rd1_en;
+	logic decode_reg_wr_en;
+
+
+
 	logic rd_mem_en;
 
 	assign exec_if.rd_ram_en = rd_mem_en;
@@ -38,11 +47,11 @@ module exec_unit (
 
 	assign regfile_interface.clk = exec_if.clk;
 	assign regfile_interface.reset_n = exec_if.reset_n;
-	assign regfile_interface.rd0_en = reg_rd0_en;
-	assign regfile_interface.rd0_addr = reg_rd0_addr;
+	assign regfile_interface.rd0_en = decode_reg_rd0_en;  //reg_rd0_en;
+	assign regfile_interface.rd0_addr = decode_reg_rd0_addr;  //reg_rd0_addr;
 	assign regfile_interface.rd0_data = regfile_rd0_data;
-	assign regfile_interface.rd1_en = reg_rd1_en;
-	assign regfile_interface.rd1_addr = reg_rd1_addr;
+	assign regfile_interface.rd1_en = decode_reg_rd1_en; //reg_rd1_en;
+	assign regfile_interface.rd1_addr = decode_reg_rd1_addr; //reg_rd1_addr;
 	assign regfile_interface.rd1_data = regfile_rd1_data;
 	assign regfile_interface.wr_en = reg_wr_en;
 	assign regfile_interface.wr_addr = reg_wr_addr;
@@ -106,7 +115,13 @@ module exec_unit (
 	decode_stage decode(
 		.clk(exec_if.clk),
 		.reset_n(exec_if.reset_n),
-		.fetched_inst(fetched_inst)
+		.fetched_inst(fetched_inst),
+		.reg_rd0_addr(decode_reg_rd0_addr),
+		.reg_rd1_addr(decode_reg_rd1_addr),
+		.reg_wr_addr(decode_reg_wr_addr),
+		.reg_rd0_en(decode_reg_rd0_en),
+		.reg_rd1_en(decode_reg_rd1_en),
+		.reg_wr_en(decode_reg_wr_en)
 	);
 
 
@@ -181,14 +196,37 @@ endmodule
 module decode_stage(
 	input logic clk,
 	input logic reset_n,
-	input	logic [31:0] fetched_inst
+	input	logic [31:0] fetched_inst,
+
+	output logic [4:0] reg_rd0_addr,
+	output logic [4:0] reg_rd1_addr,
+	output logic [4:0] reg_wr_addr,
+	output logic reg_rd0_en,
+	output logic reg_rd1_en,
+	output logic reg_wr_en
 );
 
 	bit is_reg_imm_inst;
+	bit [11:0] inst_imm;
 
 	assign is_reg_imm_inst = fetched_inst[6:0] == 7'b0010011;
 
 	always @(posedge clk) begin
-
+		if (!reset_n) begin
+			reg_rd0_addr <= 0;
+			reg_rd1_addr <= 0;
+			reg_wr_addr <= 0;
+			reg_rd0_en <= 0;
+			reg_rd1_en <= 0;
+			reg_wr_en <= 0;
+		end else begin
+			inst_imm <= is_reg_imm_inst? fetched_inst[31:20] : 0;
+			reg_rd0_addr <= 0;
+			reg_rd1_addr <= is_reg_imm_inst? fetched_inst[19:15] : 0;
+			reg_wr_addr <= is_reg_imm_inst? fetched_inst[11:7] : 0;
+			reg_rd0_en <= 0;
+			reg_rd1_en <= is_reg_imm_inst;
+			reg_wr_en <= is_reg_imm_inst;
+		end
 	end
 endmodule
