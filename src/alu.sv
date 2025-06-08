2f8f553fc96b68c271b37bfcf50daf1833138183
Pau Estalella <pau.estalella@gmail.com>
2025-06-08 13:25:12 +0200

    Add a register after the ALU
    Carry the ALU execution results to the next pipeline stage with
    a register.

--- src/alu.sv
+++ src/alu.sv
@@ -9,27 +9,24 @@ module alu_stage(
 	input logic reset_n,
 
 	input ID_EX id_ex_reg,
-	input logic [31:0] regfile_rd0_data,
-	input logic [31:0] regfile_rd1_data,
-
-	output bit result_ready,
-	output logic [31:0] alu_result
+	output 	EX_WB ex_wb_reg
 );
 
 	always @(posedge clk or negedge reset_n) begin
 		if (!reset_n) begin
-			alu_result <= '0;
-			result_ready <= 0;
+			ex_wb_reg <= '{default:0};
 		end else begin
 			if (id_ex_reg.alu_op == ALU_NONE) begin
-				alu_result <= '0;
-				result_ready <= 0;
+				ex_wb_reg.alu_result <= '0;
+				ex_wb_reg.alu_result_ready <= 0;
 			end else if (id_ex_reg.alu_op == ALU_ADD) begin
-				alu_result <=  id_ex_reg.input_a_is_immediate? id_ex_reg.inst_imm + regfile_rd1_data : 0;
-				result_ready <= 1;
+				ex_wb_reg.alu_result <=  id_ex_reg.input_a_is_immediate? id_ex_reg.inst_imm + id_ex_reg.alu_reg_input_b : 0;
+				ex_wb_reg.reg_wr_addr <= id_ex_reg.reg_wr_addr;
+				ex_wb_reg.reg_wr_en <= id_ex_reg.reg_wr_en;
+				ex_wb_reg.alu_result_ready <= 1;
 			end else begin
-				alu_result <= 'hdeadbeef;
-				result_ready <= 0;
+				ex_wb_reg.alu_result <= 'hdeadbeef;
+				ex_wb_reg.alu_result_ready <= 0;
 			end
 		end
 	end
